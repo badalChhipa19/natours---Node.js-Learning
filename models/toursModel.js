cae7344fc549e401993d38d2aa4969564f85af0d
Badal Chhipa <badalchhipa9@gmail.com>
2025-08-05 23:56:55 +0530

    Tour modal update

--- models/toursModel.js
+++ models/toursModel.js
@@ -8,15 +8,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  durations: {
+    type: Number,
+    require: [true, 'A tour must have specified duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    require: [true, 'A tour must a Group Size defined'],
+  },
+  difficulty: {
+    type: String,
+    require: [true, 'A tour must have defined difficulty'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summery: {
+    type: String,
+    trim: true,
+    require: [true, 'A tour must have summery'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    require: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
