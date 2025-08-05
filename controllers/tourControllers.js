8ae49617cd262df3a08dc0f36e37e8644317142e
Badal Chhipa <badalchhipa9@gmail.com>
2025-08-05 22:12:35 +0530

    Delete Tour

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -78,9 +78,21 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(500).json({
-    status: 'Pending',
-    message: 'Handler yet to be configured',
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+    console.log('Request param: ', req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
