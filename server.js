d239b2eee569279ca3eea478509a2a7083b6eeba
Badal Chhipa <badalchhipa9@gmail.com>
2025-08-05 15:07:44 +0530

    DB Connect

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Get Environment.
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config();
 /**
@@ -8,7 +9,13 @@ dotenv.config();
 const app = require('./app');
 
 // Create port constant.
-const { PORT } = process.env;
+const { PORT, DB_PASSWORD } = process.env;
+const DB = process.env.DB.replace('<DB_PASSWORD>', DB_PASSWORD);
+
+// Connect with DB.
+mongoose.connect(DB).then(() => {
+  console.log('DB Connected...');
+});
 
 // Listen to server.
 app.listen(PORT, () => {
