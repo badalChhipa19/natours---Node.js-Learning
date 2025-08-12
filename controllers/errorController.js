a9450dcd30f97666e04d4cee99434e00a8889b7a
Badal Chhipa <badalchhipa9@gmail.com>
2025-08-12 23:41:31 +0530

    Enhance error handling for production and development
    Refactored errorController to provide detailed error responses in development and user-friendly messages in production. Added specific handlers for CastError, duplicate fields, and validation errors from the database. Improved structure for operational and programming errors.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,67 @@
-module.exports = (err, req, res, next) => {
-  // Set default status code to 500 if not provided.
-  const statusCode = err.statusCode || 500;
-  const status = err.status || 'error';
+const AppError = require('../utils/appError');
 
-  // Send the error response.
-  res.status(statusCode).json({
-    status,
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
     message: err.message,
-    ...(process.env.NODE_ENV === 'developer' && { stack: err.stack }),
+    stack: err.stack,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV.trim() === 'production') {
+    if (err.name === 'CastError') err = handleCastErrorDB(err);
+    if (err.code === 11000) err = handleDuplicateFieldsDB(err);
+    if (err.name === 'ValidationError') err = handleValidationErrorDB(err);
+
+    sendErrorProd(err, res);
+  }
 };
