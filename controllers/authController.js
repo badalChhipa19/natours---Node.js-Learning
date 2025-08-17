c25401d8be655301da7240d4a4b4302abb00e732
Badal Chhipa <badalchhipa9@gmail.com>
2025-08-17 09:41:18 +0530

    Add JWT authentication middleware
    Introduces a `protect` middleware to verify JWT tokens, check user existence, and ensure password validity for protected routes. Also adds a helper function for token validation using promises.

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@
  * External Dependencies
  */
 const jwt = require('jsonwebtoken');
+// const { promisify } = require('util');
 
 /**
  * Internal Dependencies
@@ -51,4 +52,58 @@ exports.login = catchAsync(async (req, res, next) => {
     status: 'success',
     token,
   });
+});
+
+const validateToken = (token, secrets) =>
+  new Promise((resolve, reject) => {
+    try {
+      const decoded = jwt.verify(token, secrets);
+      resolve(decoded);
+    } catch (error) {
+      reject(new AppError(error, 401));
+    }
+  });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401),
+    );
+  }
+
+  // 2) Verification token
+  // const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const decoded = await validateToken(token, process.env.JWT_SECRET);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401,
+      ),
+    );
+  }
+
+  // 4) Check if user changed password after the token was issued.
+  if (currentUser.isPasswordChanged(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401),
+    );
+  }
+
+  // Grant access to protected route
+  req.user = currentUser;
+
+  next();
 });
