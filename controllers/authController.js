4000aa2a7214052f40d8d713ed638be0a974477a
Badal Chhipa <badalchhipa9@gmail.com>
2025-08-20 10:29:59 +0530

    Add role to user signup and fix password field
    The signup controller now includes the user's role from the request body and corrects the password confirmation field to 'confirmPassword'.

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,7 +37,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    passwordConfirm: req.body.confirmPassword,
+    confirmPassword: req.body.confirmPassword,
+    role: req.body.role,
   });
 
   const token = signToken(newUser._id);
